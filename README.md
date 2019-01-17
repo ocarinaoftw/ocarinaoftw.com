49e292aa879be3af8b0761f428c4c5716e313b86
ocarinaoftw <46791790+ocarinaoftw@users.noreply.github.com>
2019-01-17 12:29:06 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ocarinaoftw.com
---
+++ js/ocarina-tab.js
@@ -0,0 +1,14 @@
+/* 
+
+Ingredients:
+
+1. Scale
+2. Textarea to output the tabs
+3. Directions for use 
+
+Flow
+
+1. Clicking on a note on the scale displays the corresponding tab to the textarea
+2. 
+
+*/
